dc5aa29966cc51b44a94173d6767e65318013d91
vamshe531 <t.vamshi@outlook.com>
2016-01-30 13:04:36 -0600

    Naives Bayes

--- Logistic_LDA_QDA_KNN.R
+++ Logistic_LDA_QDA_KNN.R
@@ -44,3 +44,32 @@ table(glm.pred,Direction.2005)
 mean(glm.pred!=Direction.2005) #calculates test error rate
 
 #Linear Discriminant Analysis
+library(MASS)
+lda.fit=lda(Direction~Lag1+Lag2, data=Smarket,subset=train)
+lda.fit
+lda.pred=predict(lda.fit, Smarket.2005)
+names(lda.pred)
+lda.class=lda.pred$class
+table(lda.class,Direction.2005)
+mean(lda.class==Direction.2005)
+sum(lda.pred$posterior[,1]>=0.5)
+sum(lda.pred$posterior[,1]<0.5)
+lda.pred$posterior[1:20,1]
+lda.class[1:20]
+
+#Quadratic Discriminant Analysis
+qda.fit=qda(Direction~Lag1+Lag2,data=Smarket,subset=train)
+qda.fit
+qda.class =predict(qda.fit,Smarket.2005)$class
+table(qda.class,Direction.2005)
+mean(qda.class==Direction.2005)
+
+#KNN
+library(class)
+train.X=cbind(Lag1,Lag2)[train,]
+test.X=cbind(Lag1,Lag2)[!train,]
+train.Direction=Direction[train]
+set.seed(1)
+knn.pred=knn(train.X,test.X,train.Direction,k=3)
+table(knn.pred,Direction.2005)
+mean(knn.pred==Direction.2005)
